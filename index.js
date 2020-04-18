054834acc70aecd0c969c320519e60c4bd34a3cb
Alexander <nrjwolf@gmail.com>
2020-04-19 00:44:51 +0800

    add role 'ru' in pm chat

--- index.js
+++ index.js
@@ -2,38 +2,63 @@ require('dotenv').config();
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+const config = require('./config.json');
 const botUtils = require('./src/utils/bot-utils');
 
 const BOT_TOKEN = process.env.BOT_TOKEN;
 const BOT_ID = process.env.BOT_ID;
+const GUILD_ID = process.env.GUILD_ID;
+const ROLE_ID = process.env.ROLE_ID;
 
 client.on('ready', () => {
-    console.log(client);
     console.log(`Logged in as ${client.user.tag}!`);
+    console.log(client);
     botUtils.init(client);
     botUtils.logChat("Hello, I'm alive!");
+
+    client.user.setActivity("Unity"); // just for fun
 });
 
-client.on('message', msg => {
-    console.log(msg);
+client.on('message', message => {
+    console.log(message);
+    var author = message.author;
 
-    // igoner bot messages
-    if (msg.author.id == BOT_ID) return;
+    // ignore bot messages
+    if (author.id == BOT_ID) return;
 
-    // listen rivate messages 
-    if (msg.channel.type == 'dm') {
-        msg.reply('you stink 🤮');
+    // listen private messages 
+    if (message.channel.type === 'dm') {
 
+        // Is user member of server
+        let guild = client.guilds.cache.get(GUILD_ID);
+        let memeber = guild.member(author.id);
+        if (memeber) {
+
+            //check command
+            let command = botUtils.getCommand(message)
+            if (command.text === config.commands.getRoleRu.text) {
+                // assign role 'ru'
+                let roleRu = guild.roles.cache.find(x => x.id === ROLE_ID);
+                if (!memeber.roles.cache.find(x => x.id === ROLE_ID)) {
+                    memeber.roles.add(roleRu);
+
+                    message.reply(new Discord.MessageEmbed()
+                        .setColor('#0099ff')
+                        .setTitle(config.reply.addRoleRu)
+                    );
+                }
+                else {
+                    message.reply(config.reply.alreadyHasRoleRu);
+                }
+            }
+            else {
+                message.reply(config.reply.idk);
+            }
+        }
+        else {
+            message.reply(config.reply.notMember);
+        }
     }
-    // let guild = client.guilds.get('guild ID here'),
-    //     USER_ID = '123123123';
-
-    // if (guild.member(USER_ID)) {
-    //     // there is a GuildMember with that ID
-    // }
-    // }
-    // var role = msg.mentions.guild.roles.cache.find(x => x.name == 'test')
-    // msg.member.roles.add(role);
 });
 
 client.login(BOT_TOKEN);
--- src/utils/bot-utils.js
+++ src/utils/bot-utils.js
@@ -1,7 +1,9 @@
 const LOGCHAT_ID = process.env.LOGCHAT_ID;
 var client;
 
-function init(_client) {
+const config = require('../../config.json');
+
+function init(_client, config) {
     client = _client;
 }
 
@@ -9,7 +11,21 @@ function logChat(message) {
     client.channels.cache.get(LOGCHAT_ID).send(message);
 }
 
+function getCommand(message) {
+    var command = {
+        text: '',
+        args: [0],
+    }
+    if (!message.content.startsWith(config.prefix)) return command;
+    const withoutPrefix = message.content.slice(config.prefix.length);
+    const split = withoutPrefix.split(/ +/);
+    command.text = split[0];
+    command.args = split.slice(1);
+    return command;
+}
+
 module.exports = {
     init: init,
     logChat: logChat,
+    getCommand: getCommand,
 }
